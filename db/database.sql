4e3d94dc09545662b814073eb7bf4e3f68a93853
Omar Massad <zeroed55@gmail.com>
2016-04-05 21:11:02 +0300

    Update database.sql dump

--- db/database.sql
+++ db/database.sql
@@ -1,54 +1,205 @@
--- Database: golang_gin_db
+--
+-- PostgreSQL database dump
+--
 
--- DROP DATABASE golang_gin_db;
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
 
-CREATE DATABASE golang_gin_db
-  WITH OWNER = postgres
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       LC_COLLATE = 'en_US.UTF-8'
-       LC_CTYPE = 'en_US.UTF-8'
-       CONNECTION LIMIT = -1;
+--
+-- Name: golang_gin_db; Type: DATABASE; Schema: -; Owner: postgres
+--
 
--- Table: article
+CREATE DATABASE golang_gin_db WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
 
--- DROP TABLE article;
 
-CREATE TABLE article
-(
-  id serial NOT NULL,
-  user_id integer,
-  title character varying,
-  content text,
-  updated_at integer,
-  created_at integer,
-  CONSTRAINT article_id PRIMARY KEY (id),
-  CONSTRAINT article_user_id FOREIGN KEY (user_id)
-      REFERENCES "user" (id) MATCH SIMPLE
-      ON UPDATE CASCADE ON DELETE CASCADE
-)
-WITH (
-  OIDS=FALSE
+ALTER DATABASE golang_gin_db OWNER TO postgres;
+
+\connect golang_gin_db
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: article; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE article (
+    id integer NOT NULL,
+    user_id integer,
+    title character varying,
+    content text,
+    updated_at integer,
+    created_at integer
 );
-ALTER TABLE article
-  OWNER TO postgres;
 
-  -- Table: "user"
 
-  -- DROP TABLE "user";
+ALTER TABLE article OWNER TO postgres;
 
-  CREATE TABLE "user"
-  (
-    id serial NOT NULL,
+--
+-- Name: article_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE article_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE article_id_seq OWNER TO postgres;
+
+--
+-- Name: article_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE article_id_seq OWNED BY article.id;
+
+
+--
+-- Name: user; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE "user" (
+    id integer NOT NULL,
     email character varying,
     password character varying,
     name character varying,
     updated_at integer,
-    created_at integer,
-    CONSTRAINT user_id PRIMARY KEY (id)
-  )
-  WITH (
-    OIDS=FALSE
-  );
-  ALTER TABLE "user"
-    OWNER TO postgres;
+    created_at integer
+);
+
+
+ALTER TABLE "user" OWNER TO postgres;
+
+--
+-- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE user_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE user_id_seq OWNER TO postgres;
+
+--
+-- Name: user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE user_id_seq OWNED BY "user".id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY article ALTER COLUMN id SET DEFAULT nextval('article_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY "user" ALTER COLUMN id SET DEFAULT nextval('user_id_seq'::regclass);
+
+
+--
+-- Data for Name: article; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY article (id, user_id, title, content, updated_at, created_at) FROM stdin;
+\.
+
+
+--
+-- Name: article_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('article_id_seq', 1, false);
+
+
+--
+-- Data for Name: user; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY "user" (id, email, password, name, updated_at, created_at) FROM stdin;
+\.
+
+
+--
+-- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('user_id_seq', 1, false);
+
+
+--
+-- Name: article_id; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY article
+    ADD CONSTRAINT article_id PRIMARY KEY (id);
+
+
+--
+-- Name: user_id; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY "user"
+    ADD CONSTRAINT user_id PRIMARY KEY (id);
+
+
+--
+-- Name: article_user_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY article
+    ADD CONSTRAINT article_user_id FOREIGN KEY (user_id) REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: omar
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM omar;
+GRANT ALL ON SCHEMA public TO omar;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
