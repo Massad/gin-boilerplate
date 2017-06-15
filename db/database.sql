76d10bb455bd06f1b4b3b27a43b1cfd5f3635ca6
Omar Massad <omar@clearmove.com>
2017-06-15 22:11:56 +0300

    Update database with trigger functions

--- db/database.sql
+++ db/database.sql
@@ -2,33 +2,22 @@
 -- PostgreSQL database dump
 --
 
+-- Dumped from database version 9.6.1
+-- Dumped by pg_dump version 9.6.1
+
+-- Started on 2017-06-15 22:08:47 EEST
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
+SET row_security = off;
 
 --
--- Name: golang_gin_db; Type: DATABASE; Schema: -; Owner: postgres
---
-DROP DATABASE golang_gin_db;
-
-CREATE DATABASE golang_gin_db WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
-
-
-ALTER DATABASE golang_gin_db OWNER TO postgres;
-
-\connect golang_gin_db
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
---
+-- TOC entry 1 (class 3079 OID 12655)
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
 --
 
@@ -36,6 +25,8 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
+-- TOC entry 2415 (class 0 OID 0)
+-- Dependencies: 1
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
 --
 
@@ -44,12 +35,52 @@ COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 SET search_path = public, pg_catalog;
 
+--
+-- TOC entry 189 (class 1255 OID 36645)
+-- Name: created_at_column(); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION created_at_column() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+
+BEGIN
+	NEW.updated_at = EXTRACT(EPOCH FROM NOW());
+	NEW.created_at = EXTRACT(EPOCH FROM NOW());
+    RETURN NEW;
+END;
+
+$$;
+
+
+ALTER FUNCTION public.created_at_column() OWNER TO postgres;
+
+--
+-- TOC entry 190 (class 1255 OID 36646)
+-- Name: update_at_column(); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION update_at_column() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+
+BEGIN
+    NEW.updated_at = EXTRACT(EPOCH FROM NOW());
+    RETURN NEW;
+END;
+
+$$;
+
+
+ALTER FUNCTION public.update_at_column() OWNER TO postgres;
+
 SET default_tablespace = '';
 
 SET default_with_oids = false;
 
 --
--- Name: article; Type: TABLE; Schema: public; Owner: postgres; Tablespace:
+-- TOC entry 185 (class 1259 OID 17987)
+-- Name: article; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE article (
@@ -65,6 +96,7 @@ CREATE TABLE article (
 ALTER TABLE article OWNER TO postgres;
 
 --
+-- TOC entry 186 (class 1259 OID 17993)
 -- Name: article_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -79,6 +111,8 @@ CREATE SEQUENCE article_id_seq
 ALTER TABLE article_id_seq OWNER TO postgres;
 
 --
+-- TOC entry 2416 (class 0 OID 0)
+-- Dependencies: 186
 -- Name: article_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -86,7 +120,8 @@ ALTER SEQUENCE article_id_seq OWNED BY article.id;
 
 
 --
--- Name: user; Type: TABLE; Schema: public; Owner: postgres; Tablespace:
+-- TOC entry 187 (class 1259 OID 17995)
+-- Name: user; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE "user" (
@@ -102,6 +137,7 @@ CREATE TABLE "user" (
 ALTER TABLE "user" OWNER TO postgres;
 
 --
+-- TOC entry 188 (class 1259 OID 18001)
 -- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -116,6 +152,8 @@ CREATE SEQUENCE user_id_seq
 ALTER TABLE user_id_seq OWNER TO postgres;
 
 --
+-- TOC entry 2417 (class 0 OID 0)
+-- Dependencies: 188
 -- Name: user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -123,20 +161,24 @@ ALTER SEQUENCE user_id_seq OWNED BY "user".id;
 
 
 --
--- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
+-- TOC entry 2277 (class 2604 OID 18003)
+-- Name: article id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY article ALTER COLUMN id SET DEFAULT nextval('article_id_seq'::regclass);
 
 
 --
--- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
+-- TOC entry 2278 (class 2604 OID 18004)
+-- Name: user id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY "user" ALTER COLUMN id SET DEFAULT nextval('user_id_seq'::regclass);
 
 
 --
+-- TOC entry 2405 (class 0 OID 17987)
+-- Dependencies: 185
 -- Data for Name: article; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -145,13 +187,17 @@ COPY article (id, user_id, title, content, updated_at, created_at) FROM stdin;
 
 
 --
+-- TOC entry 2418 (class 0 OID 0)
+-- Dependencies: 186
 -- Name: article_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('article_id_seq', 1, false);
+SELECT pg_catalog.setval('article_id_seq', 2, true);
 
 
 --
+-- TOC entry 2407 (class 0 OID 17995)
+-- Dependencies: 187
 -- Data for Name: user; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -160,14 +206,17 @@ COPY "user" (id, email, password, name, updated_at, created_at) FROM stdin;
 
 
 --
+-- TOC entry 2419 (class 0 OID 0)
+-- Dependencies: 188
 -- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('user_id_seq', 1, false);
+SELECT pg_catalog.setval('user_id_seq', 2, true);
 
 
 --
--- Name: article_id; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+-- TOC entry 2280 (class 2606 OID 18006)
+-- Name: article article_id; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY article
@@ -175,7 +224,8 @@ ALTER TABLE ONLY article
 
 
 --
--- Name: user_id; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+-- TOC entry 2282 (class 2606 OID 18008)
+-- Name: user user_id; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY "user"
@@ -183,12 +233,44 @@ ALTER TABLE ONLY "user"
 
 
 --
--- Name: article_user_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 2284 (class 2620 OID 36647)
+-- Name: article create_article_created_at; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER create_article_created_at BEFORE INSERT ON article FOR EACH ROW EXECUTE PROCEDURE created_at_column();
+
+
+--
+-- TOC entry 2286 (class 2620 OID 36653)
+-- Name: user create_user_created_at; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER create_user_created_at BEFORE INSERT ON "user" FOR EACH ROW EXECUTE PROCEDURE created_at_column();
+
+
+--
+-- TOC entry 2285 (class 2620 OID 36648)
+-- Name: article update_article_updated_at; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER update_article_updated_at BEFORE UPDATE ON article FOR EACH ROW EXECUTE PROCEDURE update_at_column();
+
+
+--
+-- TOC entry 2287 (class 2620 OID 36654)
+-- Name: user update_user_updated_at; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER update_user_updated_at BEFORE UPDATE ON "user" FOR EACH ROW EXECUTE PROCEDURE update_at_column();
+
+
+--
+-- TOC entry 2283 (class 2606 OID 18009)
+-- Name: article article_user_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY article
     ADD CONSTRAINT article_user_id FOREIGN KEY (user_id) REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
 
 --
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
@@ -199,6 +281,8 @@ REVOKE ALL ON SCHEMA public FROM postgres;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
+
+-- Completed on 2017-06-15 22:08:47 EEST
 
 --
 -- PostgreSQL database dump complete
--- models/article.go
+++ models/article.go
@@ -34,26 +34,26 @@ func (m ArticleModel) Create(userID int64, form forms.ArticleForm) (articleID in
 		return 0, errors.New("User doesn't exist")
 	}
 
-	_, err = getDb.Exec("INSERT INTO article(user_id, title, content, updated_at, created_at) VALUES($1, $2, $3, $4, $5) RETURNING id", userID, form.Title, form.Content, time.Now().Unix(), time.Now().Unix())
+	_, err = getDb.Exec("INSERT INTO public.article(user_id, title, content, updated_at, created_at) VALUES($1, $2, $3, $4, $5) RETURNING id", userID, form.Title, form.Content, time.Now().Unix(), time.Now().Unix())
 
 	if err != nil {
 		return 0, err
 	}
 
-	articleID, err = getDb.SelectInt("SELECT id FROM article WHERE user_id=$1 ORDER BY id DESC LIMIT 1", userID)
+	articleID, err = getDb.SelectInt("SELECT id FROM public.article WHERE user_id=$1 ORDER BY id DESC LIMIT 1", userID)
 
 	return articleID, err
 }
 
 //One ...
 func (m ArticleModel) One(userID, id int64) (article Article, err error) {
-	err = db.GetDB().SelectOne(&article, "SELECT a.id, a.title, a.content, a.updated_at, a.created_at, json_build_object('id', u.id, 'name', u.name, 'email', u.email) AS user FROM article a LEFT JOIN public.user u ON a.user_id = u.id WHERE a.user_id=$1 AND a.id=$2 GROUP BY a.id, a.title, a.content, a.updated_at, a.created_at, u.id, u.name, u.email LIMIT 1", userID, id)
+	err = db.GetDB().SelectOne(&article, "SELECT a.id, a.title, a.content, a.updated_at, a.created_at, json_build_object('id', u.id, 'name', u.name, 'email', u.email) AS user FROM public.article a LEFT JOIN public.user u ON a.user_id = u.id WHERE a.user_id=$1 AND a.id=$2 LIMIT 1", userID, id)
 	return article, err
 }
 
 //All ...
 func (m ArticleModel) All(userID int64) (articles []Article, err error) {
-	_, err = db.GetDB().Select(&articles, "SELECT a.id, a.title, a.content, a.updated_at, a.created_at, json_build_object('id', u.id, 'name', u.name, 'email', u.email) AS user FROM article a LEFT JOIN public.user u ON a.user_id = u.id WHERE a.user_id=$1 GROUP BY a.id, a.title, a.content, a.updated_at, a.created_at, u.id, u.name, u.email ORDER BY a.id DESC", userID)
+	_, err = db.GetDB().Select(&articles, "SELECT a.id, a.title, a.content, a.updated_at, a.created_at, json_build_object('id', u.id, 'name', u.name, 'email', u.email) AS user FROM public.article a LEFT JOIN public.user u ON a.user_id = u.id WHERE a.user_id=$1 ORDER BY a.id DESC", userID)
 	return articles, err
 }
 
@@ -65,7 +65,7 @@ func (m ArticleModel) Update(userID int64, id int64, form forms.ArticleForm) (er
 		return errors.New("Article not found")
 	}
 
-	_, err = db.GetDB().Exec("UPDATE article SET title=$1, content=$2, updated_at=$3 WHERE id=$4", form.Title, form.Content, time.Now().Unix(), id)
+	_, err = db.GetDB().Exec("UPDATE public.article SET title=$1, content=$2, updated_at=$3 WHERE id=$4", form.Title, form.Content, time.Now().Unix(), id)
 
 	return err
 }
@@ -78,7 +78,7 @@ func (m ArticleModel) Delete(userID, id int64) (err error) {
 		return errors.New("Article not found")
 	}
 
-	_, err = db.GetDB().Exec("DELETE FROM article WHERE id=$1", id)
+	_, err = db.GetDB().Exec("DELETE FROM public.article WHERE id=$1", id)
 
 	return err
 }
